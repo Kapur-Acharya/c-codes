9dad338935fab745254652268f2254ecd0c0e0fd
Kapur Acharya <kapur.builds@gmail.com>
2025-07-13 07:33:40 +0545

    intitate commit

---
+++ problem.cpp
@@ -0,0 +1,1 @@
+// write a c++ program that takes a number form the user 1, 2 and 3 and prints a msg based on the number using a switch statement; 1: u selected , 2: u selected number 2, 3: u selected number 3
