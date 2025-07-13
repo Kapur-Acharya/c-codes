0c83c42af641703cf448d95082f7972b5037c2ec
Kapur Acharya <kapur.builds@gmail.com>
2025-07-13 07:42:45 +0545

    initiate commit

--- problem.cpp
+++ problem.cpp
@@ -1,1 +1,27 @@
 // write a c++ program that takes a number form the user 1, 2 and 3 and prints a msg based on the number using a switch statement; 1: u selected , 2: u selected number 2, 3: u selected number 3
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter 1 or 2 or 3" << endl;
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        cout << "u selected 1";
+        break;
+
+    case 2:
+        cout << "u selected 2";
+        break;
+
+    case 3:
+        cout << "u selected 3";
+        break;
+
+    default:
+        cout << "wrong input";
+        break;
+    }
+}
