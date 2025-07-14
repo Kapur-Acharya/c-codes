4b8ff2895f00d02ffb13f35edf4b581cdc89af77
Kapur Acharya <kapur.builds@gmail.com>
2025-07-14 07:49:26 +0545

    printing 10 times

--- classtask1.cpp
+++ classtask1.cpp
@@ -1,6 +1,12 @@
-// write a program to print your name using for loop
+// wap to print your name 10 times using for loop
 #include <iostream>
 using namespace std;
-int main (){
-    
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "kapur" << endl;
+    }
+    return 0;
 }
